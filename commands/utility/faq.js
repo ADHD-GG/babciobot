1bbfa3b83919889f0dea73b83891b65c617cf3a7
karohalik <karolinahalik@gmail.com>
2025-06-12 00:28:34 +0100

    Link update faq.js

--- commands/utility/faq.js
+++ commands/utility/faq.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('faq')
 		.setDescription('Najczęściej zadawane pytania dotyczące serwera'),
 	async execute(interaction) {
-		await interaction.reply('Masz pytanie? Chętnie ci pomogę, osobo wnusiowa! https://discord.com/channels/1269650292215906418/1274782836221804554/1274784828138721354');
+		await interaction.reply('Masz pytanie? Chętnie ci pomogę, osobo wnusiowa! https://discord.com/channels/1269650292215906418/1382485764948103228/1382491627574132816');
 	},
 };
