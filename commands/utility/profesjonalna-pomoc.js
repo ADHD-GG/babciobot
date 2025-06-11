167e2bc3222bf78ad0250746cfaeaec477f1e4e3
karohalik <karolinahalik@gmail.com>
2025-06-12 00:31:20 +0100

    Link update profesjonalna-pomoc.js

--- commands/utility/profesjonalna-pomoc.js
+++ commands/utility/profesjonalna-pomoc.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('profesjonalna-pomoc')
 		.setDescription('Telefony i adresy wsparcia w sytuacji kryzysowej'),
 	async execute(interaction) {
-		await interaction.reply('Osobo wnusiowa, martwię się o ciebie! Masz tu numery i adresy specjalistów: https://discord.com/channels/1269650292215906418/1269971046279151658/1277598650628968489');
+		await interaction.reply('Osobo wnusiowa, martwię się o ciebie! Masz tu numery i adresy specjalistów: https://discord.com/channels/1269650292215906418/1382485764948103228/1382491486406311956');
 	},
 };
