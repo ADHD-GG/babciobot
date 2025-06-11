58314147a5f6f3b9810ec03dc8e06c54432ad638
karohalik <karolinahalik@gmail.com>
2025-06-12 00:32:31 +0100

    Link update serwer.js

--- commands/utility/serwer.js
+++ commands/utility/serwer.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('serwer')
 		.setDescription('Informacje o serwerze'),
 	async execute(interaction) {
-		await interaction.reply('Nie opowiadałam ci tej historii! A jak opowiadałam, to powiem jeszcze raz! https://discord.com/channels/1269650292215906418/1274782836221804554/1287689416118370338');
+		await interaction.reply('Nie opowiadałam ci tej historii! A jak opowiadałam, to powiem jeszcze raz! https://discord.com/channels/1269650292215906418/1382485764948103228/1382495023672721478');
 	},
 };
