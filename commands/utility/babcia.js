dd518d8f66420d45d370c83739418744e803d917
karohalik <karolinahalik@gmail.com>
2025-06-12 00:26:44 +0100

    Link update babcia.js

--- commands/utility/babcia.js
+++ commands/utility/babcia.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('babcia')
 		.setDescription('Kim jest serwerowa babcia?'),
 	async execute(interaction) {
-		await interaction.reply('A co chcesz o mnie wiedzieć, osobo wnusiowa? https://discord.com/channels/1269650292215906418/1274782836221804554/1289909163295637657');
+		await interaction.reply('A co chcesz o mnie wiedzieć, osobo wnusiowa? https://discord.com/channels/1269650292215906418/1382485764948103228/1382495067771637810');
 	},
 };
