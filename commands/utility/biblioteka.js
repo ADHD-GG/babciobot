22cb6c858df107c75e2d614e1e830e27d6c5ac82
karohalik <karolinahalik@gmail.com>
2025-06-12 00:27:52 +0100

    Link update biblioteka.js

--- commands/utility/biblioteka.js
+++ commands/utility/biblioteka.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('biblioteka')
 		.setDescription('Materiały o neuroróżnorodności i zdrowiu psychicznym'),
 	async execute(interaction) {
-		await interaction.reply('Tu znajdziesz różne ciekawe informacje, osobo wnusiowa: https://discord.com/channels/1269650292215906418/1285662957250019461/1285663605148221473');
+		await interaction.reply('Tu znajdziesz różne ciekawe informacje, osobo wnusiowa: https://discord.com/channels/1269650292215906418/1382485764948103228/1382491539967836310');
 	},
 };
