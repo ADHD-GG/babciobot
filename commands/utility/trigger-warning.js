f62b3c138cf2776f76431604d6cb7055ff91dbb3
karohalik <karolinahalik@gmail.com>
2025-06-12 00:33:22 +0100

    Link update trigger-warning.js

--- commands/utility/trigger-warning.js
+++ commands/utility/trigger-warning.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('trigger-warning')
 		.setDescription('Instrukcje dotyczące trigger warningów oraz lista tematów'),
 	async execute(interaction) {
-		await interaction.reply('Bądź miłą osobą wnusiową i nie stresuj wrażliwych osób: https://discord.com/channels/1269650292215906418/1274782836221804554/1279140544483688468');
+		await interaction.reply('Bądź miłą osobą wnusiową i nie stresuj wrażliwych osób: https://discord.com/channels/1269650292215906418/1382485764948103228/1382492591559278704');
 	},
 };
